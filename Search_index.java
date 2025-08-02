da80775e410c1c5795e794ba28e54b659b92d148
Sai Suhaas N <saisuhaasn@gmail.com>
2025-08-02 11:26:12 +0530

    Add files via upload

---
+++ Search_index.java
@@ -0,0 +1,11 @@
+
+
+public class Search_index {
+	public static void main(String[] args) {
+		String s = "Hello_world";
+		System.out.println(s.indexOf("_"));
+		System.out.println(s.lastIndexOf("or"));
+		System.out.println(s.contains("ll"));
+	}
+
+}
---
+++ Stringimmutability.java
@@ -0,0 +1,14 @@
+
+
+public class Stringimmutabbility {
+	public static void main(String[] args) {
+		String s1="data";
+		String s2 = s1;
+		s1=s1.concat("base");
+		System.out.println(s1);
+		String s3="Hello";
+		s3.concat("world");
+		System.out.println("immutability"+" "+s3);
+	}
+
+}
